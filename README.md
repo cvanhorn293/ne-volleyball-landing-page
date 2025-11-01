d75559f4b76c04ecf6aea3ed53876e3141430762
Chris Van Horn <cvanhorn293@gmail.com>
2025-11-01 02:10:00 -0500

    Navbar updates - Lots of Navbar updates   - Added mobile menu functionality     - Menu opens and closes       - Able to click "X" and tap outside of menu to close   - Moved primary-button css to global.css   - Added core Navbar items (menu, mobile hamburger, button styling) - Imported fontawesome for icons

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 This will show the different changes done for the application. This will include assignment specific changes, what the assignment was, and when it was due. It will also have non-specific assignment updates that were done to make the landing page.
 
+## Oct 31, 2025
+
+- Lots of Navbar updates
+  - Added mobile menu functionality
+    - Menu opens and closes
+      - Able to click "X" and tap outside of menu to close
+  - Moved primary-button css to global.css
+  - Added core Navbar items (menu, mobile hamburger, button styling)
+- Imported fontawesome for icons
+
+### Assignment specific changes (Add Media Query to Landing Page: Due 11.1)
+
+- Added desktop styling to remove hamburger menu
+- Added desktop styling to remove side-menu (lives off-screen, no longer loads)
+- Removed most things from 10/30 as they were no longer relevant with the changes made
+
 ## Oct 30, 2025
 
 - More file restructure
@@ -15,6 +31,7 @@ This will show the different changes done for the application. This will include
 - Added media query for "Navbar" @ src/assets/components/navbar/navbar.css
   - reordered content using flex for desktop
   - expanded margins for nav img
+  - Removed "buy tickets" button on mobile
 
 ## Oct 28, 2025
 
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,20 +1,77 @@
+import { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import './navbar.css'
-import reactLogo from '../../assets/images/ne-logo.svg'
+import neLogo from '../../assets/images/ne-logo.svg'
 
-function Navbar() {
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+
+        if (isOpen) document.addEventListener("mousedown", handleClickOutside);
+        else document.removeEventListener("mousedown", handleClickOutside);
+
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, [isOpen]);
+    
+    useEffect(() => {
+        if (isOpen) {
+        document.body.style.overflow = "hidden";
+        } else {
+        document.body.style.overflow = "";
+        }
+    }, [isOpen]);
+
     return (
         <>
-            <nav>
-                <img src={reactLogo} alt="University of Nebraska - Official Logo"/>
-                <ul className="nav-menu">
-                    <li>Link 1</li>
-                    <li>Link 2</li>
-                    <li>Link 3</li>
+            <nav className="navbar">
+
+                <button className="hamburger" onClick={() => setIsOpen(true)}>
+                    &#9776;
+                </button>
+                
+                <img className="logo" src={neLogo} alt="University of Nebraska - Official Logo"/>
+
+                <ul className="nav-links">
+                    <li><FontAwesomeIcon icon={faHouse} size="lg" /><a href="#">Home</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Statistics</a></li>
+                    <li><a href="#">Shop</a></li>
                 </ul>
-                <button className="primary-button">Buy Tickets</button>
+
+                <button className="primary-button button-desktop-only">Buy Tickets</button>
+
+                {isOpen && <div className="overlay" />}
+
+                <div ref={menuRef} className={`side-menu ${isOpen ? "open" : ""}`}>
+                    <button className="close-btn" onClick={() => setIsOpen(false)}>
+                        &times;
+                    </button>
+
+                    <img className="side-menu-logo" src={neLogo} alt="University of Nebraska - Official Logo"/>
+
+                    <ul className="side-menu-links">
+                        <li><FontAwesomeIcon icon={faHouse} size="lg" /><a href="#" onClick={() => setIsOpen(false)}>Home</a></li>
+                        <li><a href="#" onClick={() => setIsOpen(false)}>Games</a></li>
+                        <li><a href="#" onClick={() => setIsOpen(false)}>Statistics</a></li>
+                        <li><a href="#" onClick={() => setIsOpen(false)}>Shop</a></li>
+                    </ul>
+
+                    <div className="side-menu-cta text-center">
+                        <h3>Get your tickets now!</h3>
+                        <button className="primary-button">Buy Tickets</button>
+                    </div>
+                </div>
+
+
             </nav>
         </>
     ) 
 }
-
-export default Navbar
