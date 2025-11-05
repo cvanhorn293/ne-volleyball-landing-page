4402ba7d734f7374f676c255ec70bda9d7069a32
Chris Van Horn <cvanhorn293@gmail.com>
2025-11-04 22:02:06 -0600

    Updating image paths

--- src/assets/data/carouselData.js
+++ src/assets/data/carouselData.js
@@ -1,14 +1,18 @@
+import img1 from '../images/bergen-serve.jpg';
+import img2 from '../images/team-celebrate.jpg';
+import img3 from '../images/bergen-serve.jpg'; // or another real file
+
 const slides = [
     {
         "id": 1,
-        "src": "/src/assets/images/bergen-serve.webp",
+        "src": img1,
         "alt": "Exciting Volleyball Action",
         "title": "The boring slide 1",
         "description": "Experience the excitement of volleyball with every spike and serve!"
     },
     {
         "id": 2,
-        "src": "/src/assets/images/team-celebrate.webp",
+        "src": img2,
         "alt": "Team Spirit and Unity",
         "title": "This is going to be slide 2",
         "description": "Experience the excitement of volleyball with every spike and serve!"
@@ -16,7 +20,7 @@ const slides = [
     },
     {
         "id": 3,
-        "src": "/src/assets/images/carousel3.jpg",
+        "src": img3,
         "alt": "Intense Matches",
         "title": "This is the extravagant slide 3",
         "description": "Experience the excitement of volleyball with every spike and serve!"
